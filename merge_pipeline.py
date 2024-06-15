1fa39753e2a23b6df079ef0e6b45ba6d857eec9d
nikhil <nikhildaram51@gmail.com>
2024-06-16 00:25:51 +0530

    data update

--- merge_pipeline.py
+++ merge_pipeline.py
@@ -2,6 +2,7 @@ from training_pipeline.stage1_prepare_data import prepare_data
 from training_pipeline.stage2_feature_scale import prepare_data2
 from training_pipeline.stage3_data_train import data_trainings
 from training_pipeline.stage4_evaluation import evaluation
+from prediction_pipeline.prediction_pipeline import prediction_for_batch, single_query_prediction
 
 
 if __name__ == "__main__":
@@ -9,3 +10,11 @@ if __name__ == "__main__":
     prepare_data2(data_yaml="config.yaml")
     data_trainings(data_yaml="config.yaml")
     evaluation(data_yaml="config.yaml")
+    model_data_path = r'artifacts/models/model.pkl'
+    test_tsv_path = r"artifacts/prepared/test.tsv"
+    transformer_data_path = r"artifacts/model_features/pipeline.pkl"
+    # prediction_for_batch(model_data_path,transformer_data_path, test_tsv_path )
+    query = input("Enter Your Query Here:")
+    single_query_prediction(model_data_path, transformer_data_path, query)
+
+
---
+++ prediction_pipeline/prediction_pipeline.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import numpy as np
+import joblib
+import sys, os
+sys.path.append(os.getcwd())
+from utils import *
+def single_query_prediction(model_path, transformer_path, query):
+    try:
+        model = joblib.load(model_path)
+        transformer = joblib.load(transformer_path)
+        data_query_transform = transformer.transform([query])
+        y_pred = model.predict(data_query_transform)
+        if y_pred == [1.0]:
+            print("This query is related to Python")
+        else:
+            print("This query is not related to Python")
+
+    except Exception as e:
+        raise e
+
+
+
+def prediction_for_batch(model_path, transformer_path, batch):
+    try:
+        model = joblib.load(model_path)
+        transformer = joblib.load(transformer_path)
+        test_data = get_df(batch)
+        test_data_array = np.array(test_data["Text"].str.lower().values.astype("U"))
+        metrix_test_data = transformer.transform(test_data_array)
+        y_pred_arr = np.array(model.predict(metrix_test_data))
+        test_data['prediction_data'] = y_pred_arr
+        test_data['prediction_data'] = test_data["prediction_data"].replace({
+                0.0:"Query is not related to python",
+                1.0:"Query is related to python"
+                
+        })
+        test_data.to_csv("prediction.csv", index=False)
+        return y_pred_arr
+    
+    except Exception as e:
+        raise e
+
+
+
+
+
+
+
+        
